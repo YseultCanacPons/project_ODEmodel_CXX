90749e625e3e6df2733003cc174f7a90197a046b
Yseult Canac--Pons <120583392+YseultCanacPons@users.noreply.github.com>
2025-11-26 19:30:36 +0100

    Add files via upload

---
+++ include/parametres.h
@@ -0,0 +1,18 @@
+#ifndef PARAMETRES_H
+#define PARAMETRES_H
+
+// parametres.h
+// Définit les paramètres physiques du modèle d’équations différentielles.
+
+struct Parametres
+{
+	double alpha;
+    double beta;
+    double gamma;
+    double delta;
+    double epsilon;
+    double zeta;
+    double capacite_maximale;
+};
+
+#endif
